e0086d7ab566cab859b1db272163300d62328f70
suchjose84 <suchjose@live.com>
2023-01-09 07:45:43 +0800

    template

---
+++ phpmotors/template.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="css/styles.css" media="screen">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <div id="logoImage"><img src="images/site/logo.png" alt="logoImg"></div>
+            <div id="myAccountText"><a href="">My Account</a></div>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Classic</a></li>
+                <li><a href="">Sports</a></li>
+                <li><a href="">SUV</a></li>
+                <li><a href="">Trucks</a></li>
+                <li><a href="">Used</a></li>
+
+            </ul>
+        </nav>
+        <main>
+            <h1>Content Title Here</h1>
+        </main>
+        <footer>
+            <ul>
+                <li>&#169; PHP Motors, All rights reserved.</li>
+                <li>All images used are believed to be in "Fair Use". Please notify the author if any are not
+                    and they will be removed.
+                </li>
+                <li>Last Updated: 30 March, 2018</li>
+            </ul>
+        </footer>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+    <script>
+        WebFont.load({
+            google: {
+                families: ['orbitron', 'Genos']
+            }
+        });
+    </script>
+
+</body>
+
+</html>
