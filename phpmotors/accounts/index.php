183f1a1a378b73d654b6a36b2810b1768bf43d78
suchjose84 <suchjose@live.com>
2023-01-24 07:33:15 +0800

    Week 4 Activity

---
+++ phpmotors/accounts/index.php
@@ -0,0 +1,42 @@
+<?php
+
+/* PHPMotors Accounts Controller
+         * This file is accessed at http://lvh.me/phpmotors/
+         * or at http://lvh.me/phpmotors/accounts/index.php
+         * 
+         * This file controls all traffic to the http://lvh.me/phpmotors/ URL
+*/
+
+// Get the database connection file
+require_once '../library/connections.php';
+// Get the PHP Motors model for use as needed
+require_once '../model/main-model.php';
+
+// Get the array of classifications
+$classifications = getClassifications();
+
+// Build a navigation bar using the $classifications array
+$navList = '<ul>';
+$navList .= "<li><a href='/phpmotors/index.php' title='View the PHP Motors home page'>Home</a></li>";
+foreach ($classifications as $classification) {
+    $navList .= "<li><a href='/phpmotors/index.php?action=".urlencode($classification['classificationName'])."' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
+}
+$navList .= '</ul>';
+
+$action = filter_input(INPUT_POST, 'action');
+    if ($action == NULL) {
+        $action = filter_input(INPUT_GET, 'action');
+    }        
+        
+    switch ($action) {
+        case '';
+            
+            break;
+        default:
+    }        
+
+
+
+
+
+?>
